69a9046ca34487360a3ea62e149b8b62c147549f
butterscone <77188832+butterscone@users.noreply.github.com>
2024-04-28 13:58:00 +0800

    Add new components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Nav from './Nav';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
     <p>Homepage
-      <div className="App">
-      
-      </div>
+      <>
+      <Header/>
+      <Nav/>
+      <Main/>
+      <Footer/>
+      </>
     </p>
    
   );
